eab14da21d78fa786cc1b5bff768f61578f16cdc
olsonnet2008 <olivermarkwood@gmail.com>
2015-07-09 15:09:43 +0100

    updated test case

--- src/code/woody/util/RandomPasswordGenerator.java
+++ src/code/woody/util/RandomPasswordGenerator.java
@@ -1,11 +1,21 @@
 package code.woody.util;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 /**
  *
  * Created by oliverwood on 09/07/2015.
  */
 public class RandomPasswordGenerator {
+
+    public String generatePassword(){
+        return RandomStringUtils.randomAlphanumeric(8);
+    }
+
     public static void main(String[] args) {
         System.out.println("Random Password Generator");
+        RandomPasswordGenerator rpg = new RandomPasswordGenerator();
+        System.out.print(rpg.generatePassword());
+
     }
 }
--- src/test/RandomPasswordGeneratorTest.java
+++ src/test/RandomPasswordGeneratorTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import code.woody.util.RandomPasswordGenerator;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.framework.TestCase;
@@ -12,17 +13,36 @@ import junit.framework.TestCase;
  * @version 1.0
  */
 public class RandomPasswordGeneratorTest extends TestCase {
+
+    protected String message;
+
     public RandomPasswordGeneratorTest(String name) {
         super(name);
     }
 
     public void setUp() throws Exception {
+        message = "Random Password Generator";
         super.setUp();
     }
 
     public void tearDown() throws Exception {
         super.tearDown();
     }
+
+    //@org.junit.Test
+    public void testRandomPasswordGeneratorMessage()
+    {
+        System.out.println("In test RandomPasswordGeneratorMessage");
+        assertEquals(message, "Random Password Generator");
+    }
+
+    //@org.junit.Test
+    public void testRandomPasswordGeneratorLength(){
+        System.out.println("In test RandomPasswordGeneratorLength");
+        RandomPasswordGenerator rpg = new RandomPasswordGenerator();
+        assertEquals(8,rpg.generatePassword().length());
+    }
+
 
     public static Test suite() {
         return new TestSuite(RandomPasswordGeneratorTest.class);
