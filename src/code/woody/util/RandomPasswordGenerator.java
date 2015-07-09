400a09598e246c516ac92246da813aa236ca5801
olsonnet2008 <olivermarkwood@gmail.com>
2015-07-09 14:31:54 +0100

    refactored and added test case

---
+++ src/code/woody/util/RandomPasswordGenerator.java
@@ -0,0 +1,11 @@
+package code.woody.util;
+
+/**
+ *
+ * Created by oliverwood on 09/07/2015.
+ */
+public class RandomPasswordGenerator {
+    public static void main(String[] args) {
+        System.out.println("Random Password Generator");
+    }
+}
